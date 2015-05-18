64ea5ee683be6a85cc40101e995dded49904664e
Aditya Bhosale <adityapb1546@gmail.com>
2015-05-18 21:46:50 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-os.system("wget https://bootstrap.pypa.io/get-pip.py")
-os.system("python get-pip.py")
-os.system("pip install pyenchant")
-os.system("echo 'To run use \"python main.py\" in the search folder')
